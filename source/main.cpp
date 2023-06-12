246093660273b374e0a0d32e52bbdf662f328dbc
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2023-06-12 23:27:58 +0800

    Hopefully exports main function

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,19 @@
-#include <QApplication>
+
+
+#ifdef Q_OS_ANDROID
+#  include <QGuiApplication>
+#  include <QtAndroid>
+
+// WindowManager.LayoutParams
+#  define FLAG_TRANSLUCENT_STATUS 0x04000000
+#  define FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS 0x80000000
+// View
+#  define SYSTEM_UI_FLAG_LIGHT_STATUS_BAR 0x00002000
+
+#else
+#  include <QApplication>
+#endif
+
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 #include <QUrl>
@@ -10,13 +25,19 @@
 #include "CommentTreeModel.hpp"
 #include "lib.hpp"
 
-auto main(int argc, char** argv) -> int
+Q_DECL_EXPORT int main(int argc, char* argv[])
 {
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+#ifdef Q_OS_ANDROID
+  QGuiApplication app(argc, argv);
+#else
   QApplication app(argc, argv);
+#endif
+
   QCoreApplication::setOrganizationName(QStringLiteral("B1TEMY"));
   QCoreApplication::setOrganizationDomain(QStringLiteral("b1te.my"));
   QCoreApplication::setApplicationName(QStringLiteral("Tilapia For Reddit"));
@@ -41,5 +62,24 @@ auto main(int argc, char** argv) -> int
   if (engine.rootObjects().isEmpty()) {
     return EXIT_FAILURE;
   }
+
+  // HACK to color the system bar on Android, use qtandroidextras and call the
+  // appropriate Java methods
+#ifdef Q_OS_ANDROID
+  QtAndroid::runOnAndroidThread(
+      [=]()
+      {
+        QAndroidJniObject window =
+            QtAndroid::androidActivity().callObjectMethod(
+                "getWindow", "()Landroid/view/Window;");
+        window.callMethod<void>(
+            "addFlags", "(I)V", FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.callMethod<void>("clearFlags", "(I)V", FLAG_TRANSLUCENT_STATUS);
+        window.callMethod<void>(
+            "setStatusBarColor", "(I)V", QColor("#2196f3").rgba());
+        window.callMethod<void>(
+            "setNavigationBarColor", "(I)V", QColor("#2196f3").rgba());
+      });
+#endif
   return app.exec();
 }
